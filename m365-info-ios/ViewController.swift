a043fb0d2446ff09dd7e8523de8f51f33e0de14a
Michal Jach <michal@jach.me>
2019-07-11 23:09:42 +0200

    Initial Commit

---
+++ m365-info-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m365-info-ios
+//
+//  Created by Michał Jach on 11/07/2019.
+//  Copyright © 2019 Michał Jach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
